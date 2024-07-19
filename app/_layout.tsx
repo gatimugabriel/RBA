041eb4eb764882f8d64edbaba45a09e68b636f98
Develper-ke <antonytorotich@gmail.com>
2024-07-19 09:51:18 +0300

    made changes to UI/UX and added the signup page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,10 +32,10 @@ export default function RootLayout() {
 
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="signin" options={{ headerShown: false }} />
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
 
-        {/*<Stack.Screen name="(tabs)" options={{ headerShown: false }} />*/}
-        <Stack.Screen name="+not-found" />
-
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="+not-found" /> */}
       </Stack>
     </ThemeProvider>
   );
